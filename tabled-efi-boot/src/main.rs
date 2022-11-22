e242e24b7829a610f0d5d82f58e1a28f8f220e6e
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-11-22 17:29:24 -0500

    string nonsense

--- tabled-efi-boot/src/main.rs
+++ tabled-efi-boot/src/main.rs
@@ -5,7 +5,7 @@
 //use log::info;
 
 use uefi::prelude::*;
-use uefi_services::println;
+use uefi_services::{println, print};
 
 
 #[entry]
@@ -20,13 +20,25 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
       let addr_val: u64 = if ! config_entry.address.is_null() {
         unsafe { *(config_entry.address as *const u64 ) }
       } else { 0 };
-      println!("EFI> {} = {:p} = {:#08x}", config_entry.guid, config_entry.address, addr_val);
+      print!("EFI> {} = {:p} = {:#08x} = ", config_entry.guid, config_entry.address, addr_val);
+      let mut str_addr = config_entry.address as *const u8;
+      loop {
+        if str_addr.is_null() {
+          break;
+        }
+        if unsafe { *str_addr } == 0u8 {
+          break;
+        }
+        print!("{}", unsafe { (*str_addr) as char } );
+        str_addr = unsafe { str_addr.add(1) };
+      }
+      println!();
     }
 
 
     println!("Hello world!");
 
-    system_table.boot_services().stall(18_000_000);
+    system_table.boot_services().stall(99_000_000);
 
     Status::SUCCESS
 }
